019c55a952f0c242b8c062a24611caf6f13f3cfe
George Tucker <gjt@google.com>
2018-06-29 13:25:52 -0700

    Add license headers

--- baselines/a2c/run_a2c.sh
+++ baselines/a2c/run_a2c.sh
@@ -1,3 +1,9 @@
+# Copyright 2018 Google LLC
+# 
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
 #!/bin/bash
 for SEED in `seq 1 5`
 do
